b123fcbd3b349fd7d7348d367015f325d3608238
PiotrPodobinski <piotr.pod2005@gmail.com>
2026-02-22 16:39:01 +0100

    Update PSI_zaj1_zadanie2.R

--- PSI_zaj1_zadanie2.R
+++ PSI_zaj1_zadanie2.R
@@ -3,4 +3,11 @@
 przelicz_walute=function(kwota, kurs = 4.32){
   PLN=kwota*kurs
   return(PLN)
+
 }
+# 1. Ile otrzymasz PLN za 100 EUR przy domyślnym kursie?
+wynik_domyslny = przelicz_walute(100)
+print(wynik_domyslny)
+# 2. Ile otrzymasz PLN za 100 EUR przy kursie 4.50?
+wynik_zmieniony = przelicz_walute(100, 4.50)
+print(wynik_zmieniony)
